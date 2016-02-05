6e179f772dc78602d9b38a47f025bedcc9825fdf
Kaito Watanabe <kalbus1892@gmail.com>
2016-02-05 14:29:25 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require 'uri'
+require 'net/http'
+require 'json'
+
+
+get '/' do
+  erb :index
+end
+
+get '/kana' do
+  @result = Romaji.romaji2kana params[:romaji]
+  erb :show
+end
+
+get '/football' do
+  uri = URI.parse('http://api.football-data.org/v1/teams/5/players')
+  json = Net::HTTP.get(uri)
+  result = JSON.parse(json)
+  p result
+  players = result['players'].map { |e| e['name'] }
+  players
+end
